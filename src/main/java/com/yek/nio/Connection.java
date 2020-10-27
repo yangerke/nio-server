f22443599f0546e9487aff5ea96272ed51fc8275
yangerke <yekyan@qq.com>
2020-10-27 18:11:18 +0800

    test

--- src/main/java/com/yek/nio/Connection.java
+++ src/main/java/com/yek/nio/Connection.java
@@ -1,6 +1,11 @@
 package com.yek.nio;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
 import java.nio.ByteBuffer;
+import java.nio.channels.ClosedChannelException;
 import java.nio.channels.SelectionKey;
 import java.nio.channels.SocketChannel;
 import java.util.HashMap;
@@ -11,9 +16,9 @@ public class Connection {
     private ByteBuffer sendBuffer;
     protected SelectionKey selectionKey;
     private Object userData;
-    private LinkedList<byte[]> packetQueue = new LinkedList<byte[]>();
+    private LinkedList<byte[]> packetQueue = new LinkedList<byte[]> ();
 
-    Server server;
+    Server theServer;
     SocketChannel socketChannel;
     boolean isConnected;
     boolean isClosing;
@@ -24,6 +29,8 @@ public class Connection {
     HashMap<Integer, Object> seqRelated;
     private int connectionProtocolVersion;
 
+    private static Logger logger = LoggerFactory.getLogger(Connection.class);
+
     public static final int ERR_NONE = 0;
     public static final int ERR_SERIALIZATION = -1;
     public static final int ERR_NOROOM = -2;
@@ -31,19 +38,301 @@ public class Connection {
     public static final int ERR_OUTOFMEMORY = -4;
     public static final int ERR_DISCONNECTED = -5;
 
-    Connection() {
+    Connection () {
     }
 
-    public <T> T getSeqRelated(int seq) {
+    @SuppressWarnings("unchecked")
+    public <T> T getSeqRelated (int seq) {
         if (seqRelated == null)
             return null;
-        return (T)seqRelated.get(seq);
+        return (T)seqRelated.get (seq);
     }
 
-    public <T> void setSeqRelated(int seq, T obj) {
+    @SuppressWarnings("unchecked")
+    public <T> T removeSeqRelated (int seq) {
+        if (seqRelated == null)
+            return null;
+        return (T)seqRelated.remove (seq);
+    }
+
+    public <T> void setSeqRelated (int seq, T obj) {
         if (seqRelated == null) {
             seqRelated = new HashMap<Integer, Object> ();
         }
         seqRelated.put (seq, obj);
+    }
+
+    public void setSeq (int seq) {
+        this.seq = seq;
+    }
+
+    public int getSeq () {
+        return seq;
+    }
+
+    public boolean isConnected () {
+        return this.isConnected;
+    }
+
+    public void setKeepAliveTime (int time) {
+        keepAliveTime = time;
+    }
+
+    public SocketChannel getSocketChannel () {
+        return socketChannel;
+    }
+
+    public void init (SocketChannel channel, SelectionKey key, int recvBufferSize, int sendBufferSize) {
+        socketChannel = channel;
+        selectionKey = key;
+        recvBuffer = ByteBuffer.allocate (recvBufferSize);
+        recvBuffer.clear();
+        sendBuffer = ByteBuffer.allocate (sendBufferSize);
+        sendBuffer.clear ();
+        sendBuffer.flip ();
+    }
+
+    public void close (boolean invokeDisconnectCB) {
+        if (isConnected) {
+            if (logger.isDebugEnabled ()) {
+                logger.debug ("CID[" + id + "] close " +  isConnected + " " + isConnected);
+            }
+            boolean connected = isConnected;
+            isConnected = false;
+            if (socketChannel != null) {
+                try {
+                    socketChannel.close();
+                } catch (IOException e) {
+                }
+                socketChannel = null;
+            }
+            if (selectionKey != null) {
+                selectionKey.attach (null);
+                selectionKey.cancel();
+                selectionKey = null;
+            }
+            recvBuffer = null;
+            sendBuffer = null;
+
+            if (theServer.listener != null && connected && invokeDisconnectCB) {
+                theServer.listener.handleEvent(this, IEventListener.EVENT_DISCONNECTED);
+            }
+        } else {
+            // should not go here !!!
+            if (socketChannel != null) {
+                try {
+                    socketChannel.close();
+                } catch (IOException e) {
+                }
+                socketChannel = null;
+            }
+            recvBuffer = null;
+            sendBuffer = null;
+            if (selectionKey != null) {
+                selectionKey.attach (null);
+                selectionKey.cancel();
+                selectionKey = null;
+            }
+        }
+    }
+
+    private static final int SEND_NONE = 0;
+    private static final int SEND_ALL = 1;
+    private static final int SEND_SOME = 2;
+
+    private void supplyPacketData () {
+        sendBuffer.clear ();
+        while (packetQueue.size () > 0) {
+            byte[] data = packetQueue.getFirst ();
+            if (data.length > sendBuffer.remaining ()) {
+                break;
+            }
+            sendBuffer.put (data);
+            packetQueue.remove ();
+        }
+        sendBuffer.flip ();
+    }
+
+    public void requestClose () {
+        isClosing = true;
+    }
+
+    public boolean isClosing () {
+        return isClosing;
+    }
+
+    public int sendPacketToSocketChannel () throws IOException {
+        boolean active = false;
+        for (;;) {
+            if (sendBuffer.hasRemaining ()) {
+                int pos = sendBuffer.position ();
+                if (socketChannel.write(sendBuffer) == 0) {
+                    break;
+                } else {
+                    int sent = sendBuffer.position () - pos;
+                    theServer.sendBytes += sent;
+                    active = true;
+                }
+            } else {
+                if (packetQueue.size () > 0) {
+                    supplyPacketData ();
+                } else {
+                    break;
+                }
+            }
+        }
+        if (active)
+            lastActiveTimestamp = System.currentTimeMillis();
+
+        return sendBuffer.hasRemaining ()?(active?SEND_SOME:SEND_NONE):SEND_ALL;
+    }
+
+    // must be called from selection thread !!!
+    public void sendObject (Object obj) throws IOException {
+        if (!isConnected) {
+            throw new IOException ("CID[" + id + "] sendObject '" + obj + "' err:ERR_DISCONNECTED");
+        }
+        byte[] data = theServer.serialization.writePacket (this, obj);
+        if (packetQueue.size () == 0 && data.length <= sendBuffer.capacity () - sendBuffer.limit ()) {
+            sendBuffer.compact ();
+            sendBuffer.put (data);
+            sendBuffer.flip ();
+        } else {
+            packetQueue.addLast (data);
+        }
+
+        try {
+            int sendStatus = sendPacketToSocketChannel ();
+            if (sendStatus != SEND_ALL) {
+                if (logger.isDebugEnabled ()) {
+                    logger.debug ("CID[" + id + "] sendObject: wait next send");
+                }
+                selectionKey.interestOps(SelectionKey.OP_WRITE|SelectionKey.OP_READ);
+            } else {
+                if (logger.isDebugEnabled ()) {
+                    logger.debug ("CID[" + id + "] sendObject: send finish");
+                }
+                selectionKey.interestOps(SelectionKey.OP_READ);
+                if (isClosing) {
+                    close (true);
+                }
+            }
+        } catch (ClosedChannelException ex) {
+            if (logger.isDebugEnabled ()) {
+                logger.debug ("CID[" + id + "] sendObject '" + obj + "' err:ERR_DISCONNECTED in sending");
+            }
+        }
+    }
+
+    public void handleRecvEvent () throws IOException {
+        IPacketSerialization serialization = theServer.serialization;
+        for (;;) {
+            int recv = socketChannel.read (recvBuffer);
+            if (logger.isDebugEnabled ()) {
+                logger.debug ("CID[" + id + "] recv " + recv + "/" + recvBuffer.remaining ());
+            }
+            if (recv > 0) {
+                theServer.recvBytes += recv;
+                lastActiveTimestamp = System.currentTimeMillis();
+                recvBuffer.flip();
+                int limit = recvBuffer.limit();
+                for (;;) {
+                    int position = recvBuffer.position();
+                    int packetLen = serialization.peekPacketLength (recvBuffer);
+                    if (logger.isDebugEnabled ()) {
+                        logger.debug ("CID[" + id + "] serialization.peekPacketLength " + packetLen);
+                    }
+                    if (packetLen == IPacketSerialization.ERR_BAD_PACKET) {
+                        // Error
+                        close (false);
+                        if (theServer.listener != null)
+                            theServer.listener.handleEvent (this, IEventListener.EVENT_PACKET_ERROR);
+                        return;
+                    }
+                    else if (packetLen == IPacketSerialization.ERR_REQUIRE_MORE || position + packetLen > limit)
+                        break;
+                    recvBuffer.position (position);
+                    Object obj = serialization.readPacket (this, recvBuffer, packetLen);
+                    if (obj == null) {
+                        // Error
+                        close (false);
+                        if (theServer.listener != null)
+                            theServer.listener.handleEvent (this, IEventListener.EVENT_PACKET_ERROR);
+                        return;
+                    } else {
+                        if (theServer.listener != null)
+                            theServer.listener.handlePacket (this, obj);
+                    }
+                    recvBuffer.position (position + packetLen);
+                }
+                recvBuffer.compact();
+            } else if (recv == 0) {
+                break;
+            } else /* if (recv < 0) */ {
+                // remote close
+                if (logger.isDebugEnabled ()) {
+                    logger.debug ("CID[" + id + "] remote close");
+                }
+                close (true);
+                break;
+            }
+        }
+    }
+
+    public void handleSendEvent () throws IOException {
+        int sendStatus = sendPacketToSocketChannel ();
+        if (sendStatus != Connection.SEND_ALL) {
+            if (logger.isDebugEnabled ()) {
+                logger.debug ("CID[" + id + "] wait next send");
+            }
+            selectionKey.interestOps(SelectionKey.OP_WRITE|SelectionKey.OP_READ);
+        } else {
+            if (sendStatus == Connection.SEND_ALL) {
+                if (logger.isDebugEnabled ()) {
+                    logger.debug ("CID[" + id + "] send finished");
+                }
+            }
+            selectionKey.interestOps(SelectionKey.OP_READ);
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    public <T> T getUserData () {
+        return (T)userData;
+    }
+
+    public <T> void setUserData (T data) {
+        userData = data;
+    }
+
+    public long getId () {
+        return id;
+    }
+
+    public static String getEventName (int event) {
+        switch (event) {
+            case IEventListener.EVENT_CONNECTED:
+                return "EVENT_CONNECTED";
+            case IEventListener.EVENT_DISCONNECTED:
+                return "EVENT_DISCONNECTED";
+            case IEventListener.EVENT_TIMEOUT_ERROR:
+                return "EVENT_TIMEOUT_ERROR";
+            case IEventListener.EVENT_SEND_ERROR:
+                return "EVENT_SEND_ERROR";
+            case IEventListener.EVENT_RECV_ERROR:
+                return "EVENT_RECV_ERROR";
+            case IEventListener.EVENT_PACKET_ERROR:
+                return "EVENT_PACKET_ERROR";
+            default:
+                return "<Unknown Event>";
+        }
+    }
+
+    public int getConnectionProtocolVersion () {
+        return connectionProtocolVersion;
+    }
+
+    public void setConnectionProtocolVersion (int protocolVersion) {
+        this.connectionProtocolVersion = protocolVersion;
     }
 }
--- src/main/java/com/yek/nio/Server.java
+++ src/main/java/com/yek/nio/Server.java
@@ -2,82 +2,340 @@ package com.yek.nio;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.boot.logging.LogFile;
-import org.springframework.boot.logging.LogLevel;
-import org.springframework.boot.logging.LoggerConfiguration;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.net.ServerSocket;
-import java.nio.channels.SelectionKey;
-import java.nio.channels.Selector;
-import java.nio.channels.ServerSocketChannel;
-import java.nio.channels.SocketChannel;
+import java.nio.channels.*;
+import java.util.Iterator;
+import java.util.LinkedHashSet;
+import java.util.Set;
 
 public class Server {
-    private static final Logger logger = LoggerFactory.getLogger(Server.class);
     public static final int STATUS_IDLE = 0;
     public static final int STATUS_STARTING = 1;
     public static final int STATUS_LISTENING = 2;
     public static final int STATUS_CLOSING = 3;
 
-    private InetSocketAddress listenAddress;
-    private Thread listenThread;
-    private int status;
-    private Selector selector;
     private ServerSocketChannel listenChannel;
-    private Object statusLock = new Object();
+    private Selector selector;
+    private int defaultRecvSize = 65536;
+    private int defaultSendSize = 65536;
+    private long nextConnectionId;
+    IPacketSerialization serialization;
+    IEventListener listener;
+    private InetSocketAddress listenAddress;
+    private int status;
+    private Object statusLock = new Object ();
+    private Thread listenThread;
+    private int keepAliveTime = 10000000;
+    private Set<Connection> connectionSet = new LinkedHashSet<Connection>();
+    private long selectCount;
     private int selectTimeout = 1;
+    private boolean acceptConnections = true;
+    long sendBytes;
+    long recvBytes;
 
-    public void listen(InetSocketAddress address) {
-        //初始化监听地址
-        listenAddress = address;
-        //启动监听线程
-        startListenThread();
+    private static Logger logger = LoggerFactory.getLogger (Server.class);
+
+
+    public Server () {
     }
 
-    public void startListenThread() {
-        listenThread = new Thread();
-        //设置监听线程名字
-        listenThread.setName("NIOS-THREAD");
-        //启动线程
-        listenThread.start();
+    public long getTotalRecvBytes () {
+        return recvBytes;
+    }
+
+    public long getTotalSendBytes () {
+        return sendBytes;
+    }
+
+    public int getConnectionCount () {
+        return connectionSet.size ();
+    }
+
+    public void setAcceptConnections (boolean accept) {
+        if (acceptConnections != accept) {
+            acceptConnections = accept;
+            if (listenChannel != null) {
+                try {
+                    listenChannel.register (selector, acceptConnections?SelectionKey.OP_CONNECT:0);
+                } catch (ClosedChannelException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    public void setPacketListener(IEventListener l) {
+        listener = l;
+    }
+
+    public void setSerialization(IPacketSerialization s) {
+        serialization = s;
+    }
+
+    public void setSendBufferSize(int size) {
+        defaultSendSize = size;
+    }
+
+    public void setRecvBufferSize(int size) {
+        defaultRecvSize = size;
+    }
+
+    public void setKeepAliveTime(int keepAliveTime) {
+        this.keepAliveTime = keepAliveTime;
+    }
+
+    public void setSelectTimeout (int selectTimeout) {
+        this.selectTimeout = selectTimeout;
+    }
+
+    public Connection newConnection() {
+        return new Connection ();
+    }
+
+    public void handleConnectionTimeout() {
+        long ts = System.currentTimeMillis ();
+        for (Iterator<Connection> iter = connectionSet.iterator (); iter.hasNext ();) {
+            Connection conn = (Connection) iter.next ();
+
+            if (ts - conn.lastActiveTimestamp > conn.keepAliveTime || !conn.isConnected) {
+                iter.remove ();
+                if (conn.isConnected) {
+                    if (logger.isDebugEnabled ()) {
+                        logger.debug ("CID["+conn.id+"] timeout "+ (ts - conn.lastActiveTimestamp) + "/" + conn.keepAliveTime);
+                    }
+                    conn.close (false);
+                    if (listener != null) {
+                        listener.handleEvent (conn, IEventListener.EVENT_TIMEOUT_ERROR);
+                    }
+                } else {
+                    if (logger.isDebugEnabled ()) {
+                        logger.debug ("CID["+conn.id+"][CLOSED] remove ");
+                    }
+                }
+            }
+        }
+    }
+
+    public void idleTask() {
+        handleConnectionTimeout ();
+    }
+
+    public void accept (SocketChannel socketChannel) {
+        Connection conn = null;
+        try {
+            conn = newConnection ();
+            conn.theServer = this;
+            socketChannel.configureBlocking (false);
+            SelectionKey key = socketChannel.register (selector, SelectionKey.OP_READ | SelectionKey.OP_WRITE);
+            conn.init (socketChannel, key, defaultRecvSize, defaultSendSize);
+            conn.isConnected = true;
+            conn.keepAliveTime = keepAliveTime;
+            ++nextConnectionId;
+            conn.id = nextConnectionId;
+            key.attach (conn);
+            conn.lastActiveTimestamp = System.currentTimeMillis ();
+            connectionSet.add (conn);
+            if (listener != null) {
+                listener.handleEvent (conn, IEventListener.EVENT_CONNECTED);
+            }
+        } catch (Exception e) {
+            conn.close (false);
+            if (logger.isWarnEnabled ()) {
+                logger.warn ("accept Error", e);
+            }
+        }
+    }
+
+    public void handleSelectionKey(SelectionKey key) throws IOException {
+        int ops = key.readyOps ();
+        if ((ops & SelectionKey.OP_READ) == SelectionKey.OP_READ) {
+            if (logger.isDebugEnabled ()) {
+                Connection conn = (Connection) key.attachment ();
+                logger.debug ("CID["+(conn==null?-1:conn.id)+"] handleSelectionKey ["+selectCount+"]: READ");
+            }
+            Connection conn = (Connection) key.attachment ();
+            if (conn != null) {
+                try {
+                    conn.handleRecvEvent ();
+                } catch (Exception e) {
+                    conn.close (false);
+                    if (listener != null) {
+                        listener.handleEvent (conn, IEventListener.EVENT_RECV_ERROR);
+                    }
+                    if (logger.isErrorEnabled ()) {
+                        logger.error ("read Error", e);
+                    }
+                }
+            }
+        } else if ((ops & SelectionKey.OP_WRITE) == SelectionKey.OP_WRITE) {
+            if (logger.isDebugEnabled ()) {
+                Connection conn = (Connection) key.attachment ();
+                logger.debug ("CID["+(conn==null?-1:conn.id)+"] handleSelectionKey ["+selectCount+"]: WRITE");
+            }
+            Connection conn = (Connection) key.attachment ();
+            if (conn != null) {
+                try {
+                    conn.handleSendEvent ();
+                } catch (Exception e) {
+                    conn.close (false);
+                    if (listener != null) {
+                        listener.handleEvent (conn, IEventListener.EVENT_SEND_ERROR);
+                    }
+                    if (logger.isErrorEnabled ()) {
+                        logger.error ("write Error", e);
+                    }
+                }
+            }
+        } else if ((ops & SelectionKey.OP_ACCEPT) == SelectionKey.OP_ACCEPT) {
+            if (logger.isDebugEnabled ()) {
+                Connection conn = (Connection) key.attachment ();
+                logger.debug ("CID["+(conn==null?-1:conn.id)+"] handleSelectionKey ["+selectCount+"]: ACCEPT");
+            }
+            ServerSocketChannel serverChannel = (ServerSocketChannel) key.channel ();
+            SocketChannel socketChannel = serverChannel.accept ();
+            if (socketChannel != null) {
+                if (acceptConnections) {
+                    accept (socketChannel);
+                } else {
+                    socketChannel.close ();
+                }
+            }
+        }
+    }
+
+    public void listenOnce (int timeOut) throws IOException {
+        int selects = 0;
+        if (timeOut == -1) {
+            selects = selector.select ();
+        } else if (timeOut > 0) {
+            selects = selector.select (timeOut);
+        } else {
+            selects = selector.selectNow ();
+        }
+        if (selects > 0) {
+            ++selectCount;
+            Set<SelectionKey> keys = selector.selectedKeys ();
+            for (Iterator<SelectionKey> iter = keys.iterator (); iter.hasNext ();) {
+                try {
+                    SelectionKey key = iter.next ();
+                    iter.remove ();
+                    if (!key.isValid ())
+                        continue;
+                    handleSelectionKey (key);
+                } catch (Exception e) {
+                    if (logger.isErrorEnabled ()) {
+                        logger.error ("handleSelectionKey Error", e);
+                    }
+                }
+            }
+        }
+        // maybe send pending data here
+        if (listener != null)
+            listener.handleEvent (null, IEventListener.EVENT_SEND_REQUEST);
     }
 
     private class ListenThread implements Runnable {
         @Override
         public void run() {
             try {
-                //开启socket监听
-                startListening();
+                Thread.currentThread ().setName ("nio-server-thread");
+                startListening ();
                 while (status == STATUS_LISTENING) {
                     synchronized (statusLock) {
-                        if (status != STATUS_LISTENING) {
+                        if (status != STATUS_LISTENING)
                             break;
-                        }
                     }
-                    listenOnce(selectTimeout);
+                    listenOnce (selectTimeout);
+                    idleTask ();
                 }
-            } catch(Exception e) {
-                if (logger.isErrorEnabled()) {
-                    logger.error("ListenThread error");
+            } catch (Exception e) {
+                if (logger.isErrorEnabled ()) {
+                    logger.error ("ListenThread Error", e);
                 }
             }
+            stopListening ();
         }
+    }
+
+    private void stopListening() {
+        try {
+            listenChannel.close ();
+        } catch (Exception e) {
+            if (logger.isErrorEnabled ()) {
+                logger.error ("close listenChannel Error", e);
+            }
+        }
+        listenChannel = null;
+        try {
+            selector.close ();
+        } catch (Exception e) {
+            if (logger.isErrorEnabled ()) {
+                logger.error ("close selector Error", e);
+            }
+        }
+        selector = null;
+        status = STATUS_IDLE;
     }
 
     public void startListening() throws IOException {
         status = STATUS_STARTING;
-        selector = Selector.open();
-        listenChannel = ServerSocketChannel.open();
-        ServerSocket serverSocket = listenChannel.socket();
-        serverSocket.bind(listenAddress);
-        listenChannel.configureBlocking(false);
-        listenChannel.register(selector, SelectionKey.OP_ACCEPT);
+        selector = Selector.open ();
+        listenChannel = ServerSocketChannel.open ();
+        ServerSocket socket = listenChannel.socket ();
+        socket.bind (listenAddress);
+        listenChannel.configureBlocking (false);
+        listenChannel.register (selector, SelectionKey.OP_ACCEPT);
         status = STATUS_LISTENING;
     }
 
-    public void listenOnce(int selectTimeout) {
+    public void startListenThread() {
+        listenThread = new Thread (new ListenThread ());
+        listenThread.setName ("nio-server-thread");
+        listenThread.start ();
+    }
 
+    public void listen(InetSocketAddress address) {
+        listenAddress = address;
+        startListenThread ();
+    }
+
+    public void closeNoWait() {
+        if (listenThread != null) {
+            synchronized (statusLock) {
+                status = STATUS_CLOSING;
+            }
+        }
+    }
+
+    public void awaitConnectionClose (long timeout) {
+        long ts = System.currentTimeMillis ();
+        try {
+            if (selector != null)
+                selector.wakeup ();
+        } catch (Exception e) {
+        }
+        while (listenThread != null && connectionSet.size () > 0 && (timeout < 0 || (System.currentTimeMillis () - ts) < timeout)) {
+            try {
+                Thread.sleep (1);
+            } catch (InterruptedException e) {
+            }
+        }
+    }
+
+    public void close() {
+        if (listenThread != null) {
+            synchronized (statusLock) {
+                status = STATUS_CLOSING;
+            }
+            try {
+                if (selector != null)
+                    selector.wakeup ();
+                listenThread.join ();
+            } catch (InterruptedException e) {
+            }
+        }
     }
 }
